97462bf8909fe0264f5693d61ae830c179d255c5
pierre <bastola.pierre@runpod.io>
2024-03-14 16:41:11 -0500

    change GPU Type in dev

--- examples/dotnet/Program.cs
+++ examples/dotnet/Program.cs
@@ -1,20 +1,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using Pulumi;
-using Xyz = Pulumi.Xyz;
+using Runpod = Pulumi.Runpod;
 
 return await Deployment.RunAsync(() => 
 {
-    var myRandomResource = new Xyz.Random("myRandomResource", new()
+    var testNetworkStorage = new Runpod.NetworkStorage("testNetworkStorage", new()
     {
-        Length = 24,
+        Name = "testStorage1",
+        Size = 20,
+        DataCenterId = "US-NJ",
+    });
+
+    var myRandomPod = new Runpod.Pod("myRandomPod", new()
+    {
+        CloudType = "ALL",
+        NetworkVolumeId = testNetworkStorage.NetworkStorage.Apply(networkStorage => networkStorage.Id),
+        GpuCount = 1,
+        VolumeInGb = 50,
+        ContainerDiskInGb = 50,
+        MinVcpuCount = 2,
+        MinMemoryInGb = 15,
+        GpuTypeId = "NVIDIA GeForce RTX 3070",
+        Name = "RunPod Pytorch",
+        ImageName = "runpod/pytorch",
+        DockerArgs = "",
+        Ports = "8888/http",
+        VolumeMountPath = "/workspace",
+        Env = new[]
+        {
+            new Runpod.Inputs.PodEnvArgs
+            {
+                Key = "JUPYTER_PASSWORD",
+                Value = "rns1hunbsstltcpad22d",
+            },
+        },
     });
 
     return new Dictionary<string, object?>
     {
-        ["output"] = 
+        ["pod"] = 
         {
-            { "value", myRandomResource.Result },
+            { "value", myRandomPod.Pod },
+        },
+        ["networkStorage"] = 
+        {
+            { "value", testNetworkStorage.NetworkStorage },
         },
     };
 });
