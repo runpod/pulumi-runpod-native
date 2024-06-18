85a1fa9196f3e8ec68f10fe22aebc0e710ff90a5
Stuart Bourhill <stuburger@gmail.com>
2024-06-18 11:39:04 +0100

    Address whitespace formatting in typescript example.

--- examples/nodejs/index.ts
+++ examples/nodejs/index.ts
@@ -2,64 +2,54 @@ import * as pulumi from "@pulumi/pulumi";
 import * as runpod from "@runpod-infra/pulumi";
 
 const myTemplate = new runpod.Template("testTemplate", {
-  containerDiskInGb: 5,
-  dockerArgs: "python handler.py",
-  env: [
-    {
-      key: "key1",
-      value: "value1",
-    },
-    {
-      key: "key2",
-      value: "value2",
-    },
-  ],
-  imageName: "runpod/serverless-hello-world:latest",
-  isServerless: true,
-  name: "Generated Serverless Template",
-  readme: "## Hello, World!",
-  volumeInGb: 0,
+    containerDiskInGb: 5,
+    dockerArgs: "python handler.py",
+    env: [
+      {
+        key: "key1",
+        value: "value1",
+      },
+      {
+        key: "key2",
+        value: "value2",
+      },
+    ],
+    imageName: "runpod/serverless-hello-world:latest",
+    isServerless: true,
+    name: "Generated Serverless Template",
+    readme: "## Hello, World!",
+    volumeInGb: 0,
 });
-
 const testNetworkStorage = new runpod.NetworkStorage("testNetworkStorage", {
-  name: "testStorage1",
-  size: 20,
-  dataCenterId: "US-NJ",
+    name: "testStorage1",
+    size: 20,
+    dataCenterId: "US-NJ",
 });
-
 const myRandomPod = new runpod.Pod("myRandomPod", {
-  cloudType: "ALL",
-  networkVolumeId: testNetworkStorage.networkStorage.apply(
-    (networkStorage) => networkStorage.id
-  ),
-  templateId: myTemplate.id,
-  gpuCount: 1,
-  volumeInGb: 50,
-  containerDiskInGb: 50,
-  minVcpuCount: 2,
-  minMemoryInGb: 15,
-  gpuTypeId: "NVIDIA GeForce RTX 3070",
-  name: "RunPod Pytorch",
-  imageName: "runpod/pytorch",
-  dockerArgs: "",
-  ports: "8888/http",
-  volumeMountPath: "/workspace",
-  env: [
-    {
-      key: "JUPYTER_PASSWORD",
-      value: "rns1hunbsstltcpad22d",
-    },
-  ],
+    cloudType: "ALL",
+    networkVolumeId: testNetworkStorage.networkStorage.apply(networkStorage => networkStorage.id),
+    gpuCount: 1,
+    volumeInGb: 50,
+    containerDiskInGb: 50,
+    minVcpuCount: 2,
+    minMemoryInGb: 15,
+    gpuTypeId: "NVIDIA GeForce RTX 3070",
+    name: "RunPod Pytorch",
+    imageName: "runpod/pytorch",
+    dockerArgs: "",
+    ports: "8888/http",
+    volumeMountPath: "/workspace",
+    env: [{
+        key: "JUPYTER_PASSWORD",
+        value: "rns1hunbsstltcpad22d",
+    }],
 });
-
-export const pod = {
-  value: myRandomPod.pod,
-};
-
-export const networkStorage = {
-  value: testNetworkStorage.networkStorage,
-};
-
 export const template = {
-  value: myTemplate,
+    value: myTemplate.template,
+};
+export const pod = {
+    value: myRandomPod.pod,
+};
+export const networkStorage = {
+    value: testNetworkStorage.networkStorage,
 };
