5442d621fa61027f6c1e5aa3fa28af904de12a0a
pierre <pierre.bastola@runpod.io>
2024-06-18 17:33:15 -0500

    cut release

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -8,7 +8,7 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = "v1.7.3"
+VERSION = "v1.7.6-rc"
 def readme():
     try:
         with open('README.md', encoding='utf-8') as f:
