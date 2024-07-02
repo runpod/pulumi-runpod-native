c50cd681818e6d62101c4ac85b9e24e1773467a7
pierre <pierre.bastola@runpod.io>
2024-07-02 17:43:10 -0500

    Bump version to v1.9.94

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -8,7 +8,7 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = "v1.9.93"
+VERSION = "v1.9.94"
 def readme():
     try:
         with open('README.md', encoding='utf-8') as f:
